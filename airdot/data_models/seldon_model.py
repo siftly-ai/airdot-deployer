8cb1267528d53b7a4faf9adfa1db1ef9f9ac7fe1
Abhinav Singh <abhinav.singh@Abhinavs-MacBook-Pro.local>
2023-07-07 19:58:05 +0530

    added seldon base model

--- airdot/data_models/seldon_model.py
+++ airdot/data_models/seldon_model.py
@@ -3,13 +3,52 @@ from typing_extensions import Literal
 from typing import Optional
 
 class SeldonMetadata(BaseModel):
-    pass
+    labels: Optional[dict] = {"app":'seldon'}
+    name: Optional[str] = "seldon_app"
+    
+
+class SeldonAnnotations(BaseModel):
+    project_name: Optional[str] = 'seldon_model'
+    deployment_version: Optional[str] = '0'
+
+
+class SeldonContainer(BaseModel):
+    name: str
+    imagePullPolicy:Optional[str] = 'IfNotPresent'
+    resources:Optional[dict] = {
+        'requests':{
+            'cpu':'1',
+            'memory':'1M'
+        }
+    }
+
+class SeldonPodSpecs(BaseModel):
+    nodeSepector: Optional[dict]
+    containers: Optional[SeldonContainer] = {
+        'name': 'seldon_container',
+        'image': 'None',
+        'imagePullPolicy': 'IfNotPresent',
+        'resources': {
+            'requests':{
+                'cpu':'1',
+                'memory':'1M'
+            }
+
+        }
+    }
+
+class SeldonComponentSpecs(BaseModel):
+    specs: SeldonPodSpecs
 
 class SeldonSpecs(BaseModel):
-    pass
+    annotations: Optional[SeldonAnnotations]
+    name:Optional[str] = 'seldon_model'
+    predictors: Optional[SeldonComponentSpecs]
+    
 
 class SeldonGraph(BaseModel):
-    pass
+    name:Optional[str] = 'seldon_model'
+
 
 class SeldonConfiguration(BaseModel):
     apiVersion: Optional[Literal['machinelearning.seldon.io/v1', 'machinelearning.seldon.io/v1alpha2']] = 'machinelearning.seldon.io/v1'
